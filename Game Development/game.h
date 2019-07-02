44ffaf324c5e23f06a01e5e2d994e2fd9a55dcb6
clw5180 <623497281@qq.com>
2019-07-02 23:59:31 +0800

    add Game class

---
+++ Game Development/game.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	void run();
+
+private:
+	void processEvents();
+	void update();
+	void render();
+
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+};
+
